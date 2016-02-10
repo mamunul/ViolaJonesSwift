abe1ff6baf5e4e499720cf9a3d8a32d5b0b8952d
Mamunul <mamunul@mamunul.local>
2016-02-10 11:32:19 +0600

    Initial Commit

---
+++ ViolaJonesSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ViolaJonesSwift
+//
+//  Created by Mamunul on 2/10/16.
+//  Copyright © 2016 Mamunul. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
